6ac672d318aa761f6b115cd0797d2b9c4941691c
Josh Sullivan <josh@dipperstove.com>
2012-04-14 11:57:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hiphype::Application.config.secret_token = '47b5b9011450d49ec680d96204b19dc7c20e02693a11fcef5ad59a386ed03cf0c19507bed66bb6db66a2a4aea7a8dfd171511a1a2964a2dad33892f42d8429dc'
